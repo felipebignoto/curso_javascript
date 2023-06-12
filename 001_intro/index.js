b1f0a33ffad73a03fb1f53a2f99e9f407ca00cc4
Felipe-Bignoto <felipebignoto@gmail.com>
2023-06-12 15:24:44 -0300

    iniciando dia 3

--- 001_intro/index.js
+++ 001_intro/index.js
@@ -48,3 +48,70 @@ function sayHello(name){
     console.log("Hello " + name)
 }
 sayHello("Felipe")
+
+////////////////
+//operadores aritmeticos
+let num1 = 1;
+let num2 = 3;
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(num2 ** num2)
+console.log(++num1)
+
+////////////////
+//operadores de atribuição
+let num3 = num1 + num2
+console.log(num3)
+num3 += 20;
+console.log(num3)
+
+////////////////
+//operadores de coparação
+/*
+    ===
+    ==
+    !==
+    !=
+    >
+    <
+    >=
+    <=
+*/
+
+////////////////
+//operadores ternarios
+let driver = 90
+let speed = driver > 110 ? "verdadeiro" : "false"
+
+////////////////
+//operadores logicos
+/*
+and - &&
+or - ||
+not - !
+*/
+let corCliente = "white"
+let corEstoque = "black"
+let corVendida = corCliente || corEstoque
+
+////////////////
+//for in
+const myCar = {
+    make:"ford",
+    year:"2009",
+    color:"black"
+}
+
+for(let key in myCar){
+    console.log(key, myCar[key])
+}
+
+////////////////
+//for of
+const amigos = ["ana","maria"]
+for(let key of amigos){
+    console.log(key)
+}
---
+++ 002_exercicio/index.js
@@ -0,0 +1,2 @@
+let favorite = "programar"
+document.getElementById("text").innerHTML = favorite
